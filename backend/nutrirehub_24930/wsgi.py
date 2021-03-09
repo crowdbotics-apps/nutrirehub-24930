d9771eba623ea30a1d9e11567f9325309af8b0da
Crowdbotics <team@crowdbotics.com>
2021-03-09 13:22:39 +0000

    Update models

--- backend/nutrirehub_24930/wsgi.py
+++ backend/nutrirehub_24930/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nutrirehub_24930.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nutrirehub_24930.settings")
 
 application = get_wsgi_application()
